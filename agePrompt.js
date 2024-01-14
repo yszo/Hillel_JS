77db758a40a5a1e94fc94990b3958d82e1d9a22d
Yuliia Zotova <yzotova@conversationhealth.com>
2024-01-14 19:47:51 +0200

    Update agePrompt.js

--- agePrompt.js
+++ agePrompt.js
@@ -4,7 +4,7 @@ let ageCorrector = userInput % 10;
 
 if (isNaN(userInput) || userInput < 0) {
   console.error("Введено невірні дані Оновіть сторінку та спробуйте ще раз");
-} else if (( 1 < ageCorrector && ageCorrector < 5) && !((4 <= userInput && userInput < 21))) {
+} else if (( 1 < ageCorrector && ageCorrector < 5) && !((4 < userInput && userInput < 21))) {
     console.info("Вам " + userInput + " роки")
 } else if (ageCorrector == 1 && userInput != 11) {
     console.info("Вам " + userInput + " рік")
