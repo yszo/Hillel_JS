e30e275b326d99aada8deb5322a186cfe4f52bc0
Yuliia Zotova <yzotova@conversationhealth.com>
2024-01-20 17:03:09 +0200

    fix Task2

--- summary.js
+++ summary.js
@@ -1,9 +1,8 @@
 let sum = 0;
 for (let i = 1; i <= 100; i++) {
-    if ((i % 3) == 0) {
+    if (i % 3 != 0) {
         sum = sum + i;
         console.log("current number " + i);
-        i++;
-    }
+    } 
 }
  console.log("Summary = " + sum);
